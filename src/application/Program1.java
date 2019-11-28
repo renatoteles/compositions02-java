fab9c39da1e4011d8362a3d658fb806802856bc0
Renato Teles <teles.ree@hotmail.com>
2019-11-27 22:28:30 -0200

    Compositions 02 Upado

---
+++ src/application/Program1.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+		BusinessAccount account = new BusinessAccount();
+		
+	}
+
+}
